483109ac435a302c48930d1bb3e2e0654eae8c3e
JoseCarvajal1264 <jose.carvajal@udla.edu.ec>
2024-10-28 07:44:37 -0500

    data annotations

--- CarvajalJose-ExamenProgreso1/Models/Carvajal.cs
+++ CarvajalJose-ExamenProgreso1/Models/Carvajal.cs
@@ -6,12 +6,16 @@ namespace CarvajalJose_ExamenProgreso1.Models
     {
         [Key]
         public int Id { get; set; }
-
+        [MaxLength(100)]
+        [MinLength(2)]
         public string Nombre { get; set; }
 
+        [Range(0, 2.5)]
         public decimal Altura { get; set; }
 
         public bool Trabajando { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
         public DateTime Fecha { get; set; }
 
 
---
+++ CarvajalJose-ExamenProgreso1/Models/Celular.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarvajalJose_ExamenProgreso1.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        [MinLength(2)]
+        public string Modelo { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{yyyy}")]
+        public int Año { get; set; }
+
+        [MinLength(1)]
+        public decimal Precio { get; set; }
+
+    }
+}
