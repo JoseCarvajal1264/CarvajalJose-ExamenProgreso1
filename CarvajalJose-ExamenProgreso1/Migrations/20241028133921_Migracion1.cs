e482d9e4372c022a1715632aff21880d4fd2af5e
JoseCarvajal1264 <jose.carvajal@udla.edu.ec>
2024-10-28 08:48:43 -0500

    base de datos e index

---
+++ CarvajalJose-ExamenProgreso1/Migrations/20241028133921_Migracion1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarvajalJose_ExamenProgreso1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migracion1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carvajal",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", maxLength: 15, nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Altura = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Trabajando = table.Column<bool>(type: "bit", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carvajal", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Celular",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Modelo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Año = table.Column<int>(type: "int", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Celular", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Carvajal");
+
+            migrationBuilder.DropTable(
+                name: "Celular");
+        }
+    }
+}
