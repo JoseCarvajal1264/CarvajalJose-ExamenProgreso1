4a8ee72e8e358e5ae508a8c326ce4b91202928e5
JoseCarvajal1264 <jose.carvajal@udla.edu.ec>
2024-10-28 07:32:59 -0500

    datos

---
+++ CarvajalJose-ExamenProgreso1/Models/Carvajal.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarvajalJose_ExamenProgreso1.Models
+{
+    public class Carvajal
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public decimal Altura { get; set; }
+
+        public bool Trabajando { get; set; }
+        public DateTime Fecha { get; set; }
+
+
+
+    }
+}
